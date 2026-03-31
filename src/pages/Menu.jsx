ae45f3d0663ae208da061e2f5af3d35e348a9ccc
“MITCHYUGAN” <denmichigan123@gmail.com>
2026-03-31 07:12:57 +0100

    Added the logic for menu filtering to filter by category

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import Header from "../components/Header/Header";
 import menu from "../Data/data";
-
-console.log("Menu", menu);
-
-const riceItem = menu.filter((item) => item.category === "rice");
-console.log("riceItem", riceItem);
-
-const filterRice = () => {
-  
-}
+import { useEffect } from "react";
 
 const Menu = () => {
+  const [menuItems, setMenuItems] = useState([]);
+  const [category, setCategory] = useState("all");
+
+  const filterMenu = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return menu;
+    } else {
+      const filteredItems = menu.filter((item) => item.category === category);
+      setMenuItems(filteredItems);
+      return filteredItems;
+    }
+  };
+
+  useEffect(() => {
+    filterMenu(category);
+  }, [category]);
+
   return (
     <div className="menuPage">
       <Header />
@@ -19,16 +30,31 @@ const Menu = () => {
       </section>
       <div className="menuContent">
         <section className="menuContentFilter">
-          <button className="menufilterButton menufilterButtonActive">All</button>
-          <button className="menufilterButton" onClick={() => filterRice}>Rice & More</button>
-          <button className="menufilterButton">Swallow & soup</button>
-          <button className="menufilterButton">Protein</button>
-          <button className="menufilterButton">Drinks</button>
-          <button className="menufilterButton">Snacks & Ice cream</button>
-          <button className="menufilterButton">Alcohol</button>
+          <button className="menufilterButton menufilterButtonActive" onClick={() => setCategory("all")}>
+            All
+          </button>
+          <button className="menufilterButton" onClick={() => setCategory("rice_and_more")}>
+            Rice & More
+          </button>
+          <button className="menufilterButton" onClick={() => setCategory("swallow_and_soup")}>
+            Swallow & soup
+          </button>
+          <button className="menufilterButton" onClick={() => setCategory("protein")}>
+            Protein
+          </button>
+          <button className="menufilterButton" onClick={() => setCategory("drinks")}>
+            Drinks
+          </button>
+          <button className="menufilterButton" onClick={() => setCategory("snack_and_ice_cream")}>
+            Snacks & Ice cream
+          </button>
+          <button className="menufilterButton" onClick={() => setCategory("alcohol")}>
+            Alcohol
+          </button>
         </section>
         <div className="menuContentGrid">
-          {menu.map((item) => {
+
+          {menuItems.length === 0 ? "Items of this category is not available" : menuItems.map((item) => {
             return (
               <div key={item.name} className="menuContentCard">
                 <div className="menuContentCardImg">
